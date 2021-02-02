372f03d4514582f83962c79237bc9c58e1392b47
amelia <ameliawalcek@gmail.com>
2021-02-02 10:16:41 +0200

    utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,18 +1,18 @@
 import { ROWS, COLS } from '../constants/constants'
 
+export const checkIntersect = (assetCellOne, assetCellTwo) => {
+    if(assetCellOne[0] === assetCellTwo[0] && assetCellOne[1] === assetCellTwo[1]) return true
+    return false
+}
+
 export const generateRandomCell = (currentCell) => {
     const cell = [
         Math.floor(Math.random() * COLS),
         Math.floor(Math.random() * ROWS)
     ]
 
-    if (currentCell[0] === cell[0] && currentCell[1] === cell[1]) {
+    if (checkIntersect(currentCell, cell)) {
         generateRandomCell()
     }
     return cell
-}
-
-export const checkIntersect = (assetCellOne, assetCellTwo) => {
-    if(assetCellOne[0] === assetCellTwo[0] && assetCellOne[1] === assetCellTwo[1]) return true
-    return false
 }
